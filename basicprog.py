dc848132c7e8336bac8ccf005c4d83a1212aa272
Charan Teja Parusha <charanteja.parusha@gmail.com>
2023-01-31 23:09:59 -0500

    First repo change

---
+++ basicprog.py
@@ -0,0 +1,20 @@
+# Basic program in GitLab
+
+# Import necessary libraries
+import math
+
+# Define a function for calculating the square root of a number
+def square_root(number):
+    return math.sqrt(number)
+
+# Define a function for calculating the area of a circle
+def circle_area(radius):
+    return math.pi * radius**2
+
+# Call the square root function
+result = square_root(9)
+print("The square root of 9 is:", result)
+
+# Call the circle area function
+result = circle_area(5)
+print("The area of a circle with radius 5 is:", result)
